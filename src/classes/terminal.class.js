3c66cf714009b1c545a365338eeea869699392d4
Andreas Hartmann <7207638+andreas-hartmann@users.noreply.github.com>
2025-08-13 17:03:42 +0200

    Revert "Fix: Secure WebSocket Server"

--- src/classes/terminal.class.js
+++ src/classes/terminal.class.js
@@ -1,7 +1,3 @@
-const crypto = require("crypto");
-
-const TOKEN_SECRET = crypto.randomBytes(32).toString("hex");
-
 class Terminal {
     constructor(opts) {
         if (opts.role === "client") {
@@ -29,8 +25,10 @@ class Terminal {
                 }
                 this.Ipc.send("terminal_channel-"+this.port, "Resize", cols, rows);
             };
+
             // Support for custom color filters on the terminal - see #483
             let doCustomFilter = (window.isTermFilterValidated) ? true : false;
+
             // Parse & validate color filter
             if (window.isTermFilterValidated !== true && typeof window.theme.terminal.colorFilter === "object" && window.theme.terminal.colorFilter.length > 0) {
                 doCustomFilter = window.theme.terminal.colorFilter.every((step, i, a) => {
@@ -177,9 +175,8 @@ class Terminal {
 
             let sockHost = opts.host || "127.0.0.1";
             let sockPort = this.port;
-            let token = TOKEN_SECRET;
 
-            this.socket = new WebSocket(`ws://${sockHost}:${sockPort}/?token=${token}`);
+            this.socket = new WebSocket("ws://"+sockHost+":"+sockPort);
             this.socket.onopen = () => {
                 let attachAddon = new AttachAddon(this.socket);
                 this.term.loadAddon(attachAddon);
@@ -204,6 +201,7 @@ class Terminal {
                 if (d - this.lastRefit > 10000) {
                     this.fit();
                 }
+
                 // See #397
                 if (!window.settings.experimentalGlobeFeatures) return;
                 let ips = e.data.match(/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/g);
@@ -212,8 +210,6 @@ class Terminal {
                     ips.forEach(ip => {
                         window.mods.globe.addTemporaryConnectedMarker(ip);
                     });
-    });
-}
                 }
             });
 
@@ -260,12 +256,12 @@ class Terminal {
                 function gcd(a, b) {
                     return (b == 0) ? a : gcd(b, a%b);
                 }
-
                 let d = gcd(w, h);
 
                 if (d === 100) { y = 1; x = 3;}
                 // if (d === 120) y = 1;
                 if (d === 256) x = 2;
+
                 if (window.settings.termFontSize < 15) y = y - 1;
 
                 cols = cols+x;
@@ -297,40 +293,28 @@ class Terminal {
                     this.clipboard.didCopy = true;
                 },
                 paste: () => {
-                    const {clipboard} = require("electron");
-                    this.write(clipboard.readText());
+                    this.write(remote.clipboard.readText());
                     this.clipboard.didCopy = false;
                 },
                 didCopy: false
             };
-        }
 
-        else if (opts.role === "server") {
-            const Pty = require("node-pty");
-            const Websocket = require("ws").Server;
-            const {ipcMain} = require("electron");
+        } else if (opts.role === "server") {
+
+            this.Pty = require("node-pty");
+            this.Websocket = require("ws").Server;
+            this.Ipc = require("electron").ipcMain;
 
             this.renderer = null;
             this.port = opts.port || 3000;
-            this._closed = false;
 
+            this._closed = false;
             this.onclosed = () => {};
             this.onopened = () => {};
             this.onresize = () => {};
             this.ondisconnected = () => {};
 
-            this.tty = new Pty.spawn(opts.shell || "bash", (opts.params.length > 0 ? opts.params : (process.platform === "win32" ? [] : ["--login"])), {
-                name: opts.env.TERM || "xterm-256color",
-                cols: 80,
-                rows: 24,
-                cwd: opts.cwd || process.env.PWD,
-                env: opts.env || process.env
-            });
-
-            this._nextTickUpdateTtyCWD = false;
-            this._nextTickUpdateProcess = false;
             this._disableCWDtracking = false;
-
             this._getTtyCWD = tty => {
                 return new Promise((resolve, reject) => {
                     let pid = tty._pid;
@@ -358,7 +342,6 @@ class Terminal {
                     }
                 });
             };
-
             this._getTtyProcess = tty => {
                 return new Promise((resolve, reject) => {
                     let pid = tty._pid;
@@ -378,7 +361,8 @@ class Terminal {
                     }
                 });
             };
-
+            this._nextTickUpdateTtyCWD = false;
+            this._nextTickUpdateProcess = false;
             this._tick = setInterval(() => {
                 if (this._nextTickUpdateTtyCWD && this._disableCWDtracking === false) {
                     this._nextTickUpdateTtyCWD = false;
@@ -422,49 +406,79 @@ class Terminal {
                 }
             }, 1000);
 
-            this.wss = new Websocket({
-                host: "127.0.0.1",
-                port: this.port,
-                clientTracking: true,
-                verifyClient: info => {
-                    try {
-                        const url = new URL(`http://localhost${info.req.url}`);
-                        const token = url.searchParams.get("token");
-                        const isLocal = info.req.socket.remoteAddress === "127.0.0.1" || info.req.socket.remoteAddress === "::1";
-                        return isLocal && token === TOKEN_SECRET;
-                    } catch {
-                        return false;
-                    }
-                }
-            });
-
-            this.Ipc = ipcMain;
-            this.Ipc.on("terminal_channel-"+this.port, (e, ...args) => {
-                switch(args[0]) {
-                    case "Renderer startup":
-                        this.renderer = e.sender;
-                        if (!this._closed) {
-                            if (!this._disableCWDtracking && this.tty._cwd) {
-                                this.renderer.send("terminal_channel-"+this.port, "New cwd", this.tty._cwd);
-                            }
-                            if (this._disableCWDtracking) {
-                                this.renderer.send("terminal_channel-"+this.port, "Fallback cwd", opts.cwd || process.env.PWD);
-                            }
-                        }
-                        break;
-                    case "Resize":
-                        try { this.tty.resize(Number(args[1]), Number(args[2])); } catch(error) {}
-                        break;
-                }
+            this.tty = this.Pty.spawn(opts.shell || "bash", (opts.params.length > 0 ? opts.params : (process.platform === "win32" ? [] : ["--login"])), {
+                name: opts.env.TERM || "xterm-256color",
+                cols: 80,
+                rows: 24,
+                cwd: opts.cwd || process.env.PWD,
+                env: opts.env || process.env
             });
 
             this.tty.onExit((code, signal) => {
                 this._closed = true;
-                try { this.tty.kill(); } catch {}
+                this.onclosed(code, signal);
             });
-        }
 
-        else {
+            this.wss = new this.Websocket({
+                port: this.port,
+                clientTracking: true,
+                verifyClient: info => {
+                    if (this.wss.clients.length >= 1) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            });
+            this.Ipc.on("terminal_channel-"+this.port, (e, ...args) => {
+                switch(args[0]) {
+                    case "Renderer startup":
+                        this.renderer = e.sender;
+                        if (!this._disableCWDtracking && this.tty._cwd) {
+                            this.renderer.send("terminal_channel-"+this.port, "New cwd", this.tty._cwd);
+                        }
+                        if (this._disableCWDtracking) {
+                            this.renderer.send("terminal_channel-"+this.port, "Fallback cwd", opts.cwd || process.env.PWD);
+                        }
+                        break;
+                    case "Resize":
+                        let cols = args[1];
+                        let rows = args[2];
+                        try {
+                            this.tty.resize(Number(cols), Number(rows));
+                        } catch (error) {
+                            //Keep going, it'll work anyways.
+                        }
+                        this.onresized(cols, rows);
+                        break;
+                    default:
+                        return;
+                }
+            });
+            this.wss.on("connection", ws => {
+                this.onopened(this.tty._pid);
+                ws.on("close", (code, reason) => {
+                    this.ondisconnected(code, reason);
+                });
+                ws.on("message", msg => {
+                    this.tty.write(msg);
+                });
+                this.tty.onData(data => {
+                    this._nextTickUpdateTtyCWD = true;
+                    this._nextTickUpdateProcess = true;
+                    try {
+                        ws.send(data);
+                    } catch (e) {
+                        // Websocket closed
+                    }
+                });
+            });
+
+            this.close = () => {
+                this.tty.kill();
+                this._closed = true;
+            };
+        } else {
             throw "Unknown purpose";
         }
     }
